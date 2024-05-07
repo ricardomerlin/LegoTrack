02fd2317974a81db62fd0a14b7f19a86e9a9b7f4
Ricardo Merlin <rcrdmerlin@gmail.com>
2024-05-07 00:47:56 -0400

    Initial Commit

---
+++ LegoTrack/LegoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoTrackApp.swift
+//  LegoTrack
+//
+//  Created by Ricardo Merlin on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct LegoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
